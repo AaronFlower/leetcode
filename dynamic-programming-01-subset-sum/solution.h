0d63487763e6c5012fc5fea5ba838c42a562b7bd
AaronFlower <haojunzhan@gmail.com>
2018-05-16 09:04:26 +0800

    solve subset sum with dynamic programming mem

--- dynamic-programming-01-subset-sum/solution.h
+++ dynamic-programming-01-subset-sum/solution.h
@@ -2,26 +2,53 @@
 #define GREEDY_SOLUTION_H_
 
 #include<vector>
+#include<string>
+#include<unordered_map>
 
 using std::vector;
+using std::unordered_map;
+using std::string;
+using std::to_string;
 
 class Solution {
     public: 
         using pos = size_t;
+        using umap = unordered_map<string, bool>;
         bool checkIsSubsetSumRecursion(const vector<int> &arr, pos i, int total) {
             if (total == 0) return true;
             if (i == 0 && total != 0) return false;
             
-            if (total < arr[i]) {
+            if (arr[i - 1] > total) {
                 // Only has one case. 
                 return checkIsSubsetSumRecursion(arr, i - 1, total);
             } else {
                 // Has two cases.
-                return checkIsSubsetSumRecursion(arr, i - 1, total) || checkIsSubsetSumRecursion(arr, i - 1, total - arr[i]);
+                return checkIsSubsetSumRecursion(arr, i - 1, total) || checkIsSubsetSumRecursion(arr, i - 1, total - arr[i - 1]);
             }
 
             return true;
         }
+
+        bool checkIsSubsetSumMem(const vector<int> &arr, pos i, int total, umap &mem) {
+            string key = to_string(i - 1) + "|" + to_string(total); 
+            auto search = mem.find(key);
+            if (search != mem.end()) {
+                return search->second;
+            }
+            if (total == 0) return true;
+            if (i == 0 && total != 0) return false;
+            bool ret;
+            if (arr[i - 1] > total) {
+                // Only has one case. 
+                ret = checkIsSubsetSumMem(arr, i - 1, total, mem);
+            } else {
+                // Has two cases.
+                ret = checkIsSubsetSumMem(arr, i - 1, total, mem) || checkIsSubsetSumMem(arr, i - 1, total - arr[i - 1], mem);
+            }
+            mem.insert(std::make_pair(key, ret));
+            return ret;
+        }
+
 };
 
 
--- dynamic-programming-01-subset-sum/test.cpp
+++ dynamic-programming-01-subset-sum/test.cpp
@@ -2,6 +2,15 @@
 #include "gmock/gmock.h"
 #include "solution.h"
 
+#include <vector>
+#include <iostream>
+#include <string>
+
+
+using std::vector;
+using std::string;
+
+
 TEST(Solution, addOperators) {
     Solution s;
     vector<int> originSet{3, 34, 4, 12, 5, 2};
@@ -12,4 +21,37 @@ TEST(Solution, addOperators) {
     originSet = {1, 8, 2, 5};
     sum = 4;
     EXPECT_EQ(s.checkIsSubsetSumRecursion(originSet, originSet.size(), sum), false);
+
+
+    originSet = {1, 5, 11, 6};
+    sum = 42;
+    EXPECT_EQ(s.checkIsSubsetSumRecursion(originSet, originSet.size(), sum), false);
+    
+    originSet = {1, 3};
+    sum = 4;
+    EXPECT_EQ(s.checkIsSubsetSumRecursion(originSet, originSet.size(), sum), true);
+}
+
+TEST(Solution, addOperatorsMem) {
+    Solution s;
+    vector<int> originSet{3, 34, 4, 12, 5, 2};
+    int sum;
+    Solution::umap map;
+    EXPECT_EQ(s.checkIsSubsetSumMem(originSet, originSet.size(), 9, map), true);
+
+    originSet = {1, 8, 2, 5};
+    map.clear();
+    sum = 4;
+    EXPECT_EQ(s.checkIsSubsetSumMem(originSet, originSet.size(), sum, map), false);
+
+
+    originSet = {1, 5, 11, 6};
+    map.clear();
+    sum = 42;
+    EXPECT_EQ(s.checkIsSubsetSumMem(originSet, originSet.size(), sum, map), false);
+    
+    originSet = {1, 3};
+    map.clear();
+    sum = 4;
+    EXPECT_EQ(s.checkIsSubsetSumMem(originSet, originSet.size(), sum, map), true);
 }
