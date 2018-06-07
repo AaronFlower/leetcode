304caeebafb5b92d8cdfef15d82b27b931e6acb8
AaronFlower <haojunzhan@gmail.com>
2018-06-07 09:26:32 +0800

    add problem 021 merge two sorted lists

---
+++ 021-MergeTwoSortedLists/solution.h
@@ -0,0 +1,48 @@
+#ifndef LEETCODE_SOLUTION_H__
+#define LEETCODE_SOLUTION_H__ 
+
+#include <vector>
+using std::vector;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int v):val(v), next(nullptr){}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy(INT_MIN);
+        ListNode* tail = &dummy;
+
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+
+    void createLinkList(ListNode* & list,  vector<int> data) {
+        ListNode *p;
+        list = nullptr;
+        for (auto val : data) {
+            if (list == nullptr) {
+                list = new ListNode(val);
+                p = list;
+            } else {
+                p->next = new ListNode(val);
+                p = p->next;
+            } 
+        }
+    }
+};
+#endif
+
---
+++ 021-MergeTwoSortedLists/test.cpp
@@ -0,0 +1,48 @@
+#include "gtest/gtest.h"
+#include "solution.h"
+
+#include <vector>
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+void checkList(ListNode* list, const vector<int>& data) {
+    ListNode* p = list;
+    int i = 0;
+    while (p) {
+        EXPECT_EQ(p->val, data[i++]);
+        p = p->next;
+    }
+}
+
+TEST(MergeTwoLists, ByDummyHead) {
+    Solution s;
+    ListNode* list1, * list2;
+    s.createLinkList(list1, {1, 2, 4});
+    EXPECT_EQ(1, list1->val);
+    EXPECT_EQ(2, list1->next->val);
+    EXPECT_EQ(4, list1->next->next->val);
+    EXPECT_EQ(nullptr, list1->next->next->next);
+
+    s.createLinkList(list2, {1, 3, 4});
+    EXPECT_EQ(1, list2->val);
+    EXPECT_EQ(3, list2->next->val);
+    EXPECT_EQ(4, list2->next->next->val);
+    EXPECT_EQ(nullptr, list2->next->next->next);
+
+    vector<int> data = {1, 1, 2, 3, 4, 4};
+    ListNode* newList = s.mergeTwoLists(list1, list2);
+    checkList(newList, data);
+    
+    s.createLinkList(list1, {1});
+    s.createLinkList(list2, {1, 2, 3});
+    newList = s.mergeTwoLists(list1, list2);
+    checkList(newList, {1, 1, 2, 3});
+
+    s.createLinkList(list1, {1, 2, 3});
+    s.createLinkList(list2, {1});
+    newList = s.mergeTwoLists(list1, list2);
+    checkList(newList, {1, 1, 2, 3});
+}
+
