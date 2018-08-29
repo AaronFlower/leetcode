d769eeb241d965431e9fa9ca99f3f26a11c3fe8e
AaronFlower <haojunzhan@gmail.com>
2018-08-29 09:43:53 +0800

    Problem 200: add union-find by rank

--- 200-NumbersIsland/solution.h
+++ 200-NumbersIsland/solution.h
@@ -9,6 +9,12 @@ using std::endl;
 
 using std::vector;
 
+struct Subset {
+    int parent;
+    int rank;
+    Subset():parent(0),rank(0){}
+};
+
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
@@ -78,6 +84,70 @@ public:
         }
         cout << endl;
     }
+
+    int numIslandsByUnionRank(vector<vector<char>>& grid) {
+        int M = grid.size();
+        if (M == 0) return 0;
+        int N = grid[0].size();
+        if (N == 0) return 0;
+        
+        int shape= N * M;
+        Subset subsets[shape];
+        int dirX[] = {0, 0, -1, 1};
+        int dirY[] = {1, -1, 0, 0};
+        int count = 0;
+        
+        for (int i = 0; i < M; ++i) {
+            for (int j = 0; j < N; ++j) {
+                int idx = i * N + j;
+                if (grid[i][j] == '1') {
+                    subsets[idx].parent = idx;
+                    ++count;
+                } else {
+                    subsets[idx].parent = -1;
+                }
+            }
+        }
+        
+        for (int i = 0; i < M; ++i) {
+            for (int j = 0; j < N; ++j) {
+                if (grid[i][j] == '1') {
+                    int x = find(subsets, i * N + j);
+                    for (int k = 0; k < 4; ++k) {
+                        int newI = i + dirX[k];
+                        int newJ = j + dirY[k];
+                        if (newI >= 0 && newI < M && newJ >= 0 && newJ < N && grid[newI][newJ] == '1') {
+                            x = find(subsets, x);
+                            int y = find(subsets, newI * N + newJ);
+                            
+                            if (x != y) {
+                                unionSubset(subsets, x, y);
+                                --count;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+              
+        return count;
+    }
+private:
+    int find(Subset* subsets, int i) {
+        if (subsets[i].parent == i) return i;
+        return find(subsets, subsets[i].parent);
+    }
+    
+    void unionSubset(Subset* subsets, int i, int j) {
+        if (subsets[i].rank == subsets[j].rank) {
+            subsets[i].parent = j;
+            subsets[j].rank++;
+        } else if (subsets[i].rank < subsets[j].rank) {
+            subsets[i].parent = j;
+        } else {
+            subsets[j].parent = i;
+        }
+    } 
 };
 
 
--- 200-NumbersIsland/test.cpp
+++ 200-NumbersIsland/test.cpp
@@ -21,3 +21,23 @@ TEST(LinkList, Small) {
     EXPECT_EQ(s.numIslands(grid), 1);
 }
 
+TEST(LinkList, UnionFindByRank) {
+    Solution s;
+    vector<vector<char>> grid = {
+        {'1','1','1','1','0'},
+        {'1','1','0','1','0'},
+        {'1','1','0','0','0'},
+        {'0','0','0','0','0'}
+    };
+    EXPECT_EQ(s.numIslandsByUnionRank(grid), 1);
+}
+
+TEST(LinkList, UnionFindByRankSmall) {
+    Solution s;
+    vector<vector<char>> grid = {
+        {'1','1'},
+        {'0','1'}
+    };
+    EXPECT_EQ(s.numIslandsByUnionRank(grid), 1);
+}
+
