0d34601c70bc08e9a4b2af0593e5ad38fbcf40f8
AaronFlower <haojunzhan@gmail.com>
2018-10-31 14:52:31 +0800

    update trie delete method

--- x016-trie/solution.h
+++ x016-trie/solution.h
@@ -45,6 +45,34 @@ public:
         pCrawl->isEnd = true;
     }
 
+    bool deleteKey(const string& key) {
+        if (key.length()) {
+            return deleteHelper(root, key);
+        }
+        return false;
+    }
+
+    bool deleteHelper(Node *root, const string& key) {
+        if (root) {
+            // Base case
+            if (key.size() == 0) {
+                if (root->isEnd) {
+                    root->isEnd = false; // unmark the node
+                    return hasNoChildren(root);
+                }
+                return false;
+            } else {
+                int i = key[0] - 'a';
+                if (deleteHelper(root->children[i], key.substr(1))) {
+                    delete root->children[i];
+                    // recursively climb up, and delete elligible nodes.
+                    return !root->isEnd && hasNoChildren(root);
+                }
+            }
+        }
+        return false;
+    }
+
     bool search(const string &key) {
         Node *pCrawl = root;
         for (char c:key) {
@@ -67,6 +95,13 @@ private:
             }
             delete root;
         }
+    }
+
+    bool hasNoChildren(const Node* node) {
+        for (Node *child: node->children) {
+            if (child) return false;
+        }
+        return true;
     }
 
 };
--- x016-trie/test.cpp
+++ x016-trie/test.cpp
@@ -10,5 +10,40 @@ TEST(Solution, Trie) {
     Trie r({"the", "a", "there", "answer", "any", "by", "bye", "their"});
     
     EXPECT_EQ(r.search("the"), true);
+    EXPECT_EQ(r.search("th"), false);
     EXPECT_EQ(r.search("these"), false);
+    EXPECT_EQ(r.search("by"), true);
+    EXPECT_EQ(r.search("bye"), true);
+}
+
+TEST(Trie, Delete) {
+    Trie r({"by", "bye"});
+    EXPECT_EQ(r.search("by"), true);
+    EXPECT_EQ(r.search("bye"), true);
+    EXPECT_EQ(r.search("a"), false);
+
+    EXPECT_EQ(r.deleteKey(""), false);
+    EXPECT_EQ(r.deleteKey("a"), false);
+    EXPECT_EQ(r.deleteKey("bya"), false);
+    EXPECT_EQ(r.deleteKey("byab"), false);
+    EXPECT_EQ(r.deleteKey("b"), false);
+
+    EXPECT_EQ(r.search("by"), true);
+    EXPECT_EQ(r.deleteKey("by"), false);
+    EXPECT_EQ(r.search("by"), false);
+
+    /* EXPECT_EQ(r.search("bye"), true); */
+    /* EXPECT_EQ(r.deleteKey("bye"), false); */
+    /* EXPECT_EQ(r.search("bye"), false); */
+}
+
+TEST(Trie, DeleteBY) {
+    Trie r({"by"});
+    EXPECT_EQ(r.search("by"), true);
+    EXPECT_EQ(r.search("bye"), false);
+    EXPECT_EQ(r.search("a"), false);
+
+    EXPECT_EQ(r.search("by"), true);
+    EXPECT_EQ(r.deleteKey("by"), false);
+    EXPECT_EQ(r.search("by"), false);
 }
